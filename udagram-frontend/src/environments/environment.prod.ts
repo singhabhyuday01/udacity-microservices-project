725172c3a759fd94be9ea49371814f5aa12f1136
Abhyuday Singh <abhyudaysingh0111@gmail.com>
2021-09-12 22:33:22 +0530

    changed env prod of frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acb7e5103194747e5967837006096979-1336495080.ap-south-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
